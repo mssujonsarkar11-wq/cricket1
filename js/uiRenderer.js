d1a6032c2f877bf0053a4e62d78f271acc9829c9
mssujonsarkar11-wq <mssujonsarkar11@gmail.com>
2026-03-14 14:02:44 +0600

    uiRenderer.js

--- js/uiRenderer.js
+++ js/uiRenderer.js
@@ -1,1 +1,43 @@
+import { db, ref, onValue } from "./firebase.js";
+import { showAnimation } from "./animations.js";
 
+const score = document.getElementById("score");
+const overs = document.getElementById("overs");
+const batting = document.getElementById("battingScorecard");
+const partnership = document.getElementById("partnershipRuns");
+
+onValue(ref(db,"match"),snap=>{
+
+let m = snap.val()
+
+score.innerText = m.runs + "/" + m.wickets
+overs.innerText = m.overs + "." + m.balls
+
+partnership.innerText = m.partnership + " runs"
+
+renderScorecard(m)
+
+})
+
+function renderScorecard(m){
+
+batting.innerHTML=""
+
+m.batsmen.forEach(p=>{
+
+let sr = (p.runs/p.balls*100 || 0).toFixed(1)
+
+batting.innerHTML += `
+<tr>
+<td>${p.name}</td>
+<td>${p.runs}</td>
+<td>${p.balls}</td>
+<td>${p.fours}</td>
+<td>${p.sixes}</td>
+<td>${sr}</td>
+</tr>
+`
+
+})
+
+}
