20fc54f3cd19d5824b60a092c295dfc4d7d58aaf
mssujonsarkar11-wq <mssujonsarkar11@gmail.com>
2026-03-14 14:02:28 +0600

    animations.js

--- js/animations.js
+++ js/animations.js
@@ -1,1 +1,14 @@
+export function showAnimation(text){
 
+const box = document.getElementById("animation")
+
+box.innerText = text
+box.classList.add("show")
+
+setTimeout(()=>{
+
+box.classList.remove("show")
+
+},2000)
+
+}
