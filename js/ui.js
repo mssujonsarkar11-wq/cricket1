bcb7ea068d2267564d23689d4108dea9a24af970
mssujonsarkar11-wq <mssujonsarkar11@gmail.com>
2026-03-14 16:41:56 +0600

    ui.js

--- js/ui.js
+++ js/ui.js
@@ -2,16 +2,9 @@ import { db, ref, onValue } from "./firebase.js"
 
 const video=document.getElementById("camera")
 
-navigator.mediaDevices.getUserMedia({
-
-video:true,
-audio:true
-
-})
+navigator.mediaDevices.getUserMedia({video:true})
 .then(stream=>{
-
 video.srcObject=stream
-
 })
 
 onValue(ref(db,"match"),snap=>{
@@ -19,6 +12,9 @@ onValue(ref(db,"match"),snap=>{
 let m=snap.val()
 
 if(!m) return
+
+document.getElementById("teamA").innerText=m.teamA
+document.getElementById("teamB").innerText=m.teamB
 
 document.getElementById("score").innerText=
 m.runs+"-"+m.wickets
@@ -32,10 +28,15 @@ m.batsmen[0].name+" "+m.batsmen[0].runs+"("+m.batsmen[0].balls+")"
 document.getElementById("bat2").innerText=
 m.batsmen[1].name+" "+m.batsmen[1].runs+"("+m.batsmen[1].balls+")"
 
-document.getElementById("bowler").innerText=
-m.bowler.name
+document.getElementById("bowler").innerText=m.bowler.name
 
 renderBalls(m)
+
+if(m.anim){
+
+showAnim(m.anim)
+
+}
 
 })
 
@@ -50,5 +51,21 @@ m.lastBalls.forEach(b=>{
 box.innerHTML+=`<span>${b}</span>`
 
 })
+
+}
+
+function showAnim(text){
+
+let box=document.getElementById("anim")
+
+box.innerText=text
+
+box.classList.add("show")
+
+setTimeout(()=>{
+
+box.classList.remove("show")
+
+},1200)
 
 }
