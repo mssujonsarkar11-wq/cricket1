391bed5bf23b90b82329bc76b0cc7434df4c090d
mssujonsarkar11-wq <mssujonsarkar11@gmail.com>
2026-03-14 14:04:16 +0600

    firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getDatabase, ref, set, onValue } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
 
+const firebaseConfig = {
+apiKey: "AIzaSyD1UHEQxowIlbs8CykLfzyEDVg8Ygj0ZAU",
+authDomain: "cricket-score-c5c9f.firebaseapp.com",
+projectId: "cricket-score-c5c9f",
+storageBucket: "cricket-score-c5c9f.firebasestorage.app",
+messagingSenderId: "5554849346",
+appId: "1:5554849346:web:26d98739b85220fcbfcfdb"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+export { db, ref, set, onValue };
