ddcae50b5a711af602169f93e6473d80a88fe4cf
mssujonsarkar11-wq <mssujonsarkar11@gmail.com>
2026-03-14 18:54:55 +0600

    ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,97 +1,121 @@
 import { db, ref, onValue } from "./firebase.js"
 
-const video = document.getElementById("camera")
+const video=document.getElementById("camera")
 
 navigator.mediaDevices.getUserMedia({video:true})
 .then(stream=>{
-video.srcObject = stream
+video.srcObject=stream
 })
 
-onValue(ref(db,"match"), snap => {
+onValue(ref(db,"match"),snap=>{
 
-let m = snap.val()
+let m=snap.val()
 
 if(!m) return
 
-document.getElementById("teamA").innerText = m.teamA
-document.getElementById("teamB").innerText = m.teamB
+document.getElementById("teamA").innerText=m.teamA
+document.getElementById("teamB").innerText=m.teamB
 
-document.getElementById("score").innerText =
-m.runs + "-" + m.wickets
+document.getElementById("score").innerText=
+m.runs+"-"+m.wickets
 
-document.getElementById("overs").innerText =
-"(" + m.overs + "." + m.balls + ")"
+document.getElementById("overs").innerText=
+"("+m.overs+"."+m.balls+")"
 
-document.getElementById("bat1").innerText =
-m.batsmen[0].name + " " + m.batsmen[0].runs + "(" + m.batsmen[0].balls + ")"
+document.getElementById("bat1").innerText=
+m.batsmen[0].name+" "+m.batsmen[0].runs+"("+m.batsmen[0].balls+")"
 
-document.getElementById("bat2").innerText =
-m.batsmen[1].name + " " + m.batsmen[1].runs + "(" + m.batsmen[1].balls + ")"
+document.getElementById("bat2").innerText=
+m.batsmen[1].name+" "+m.batsmen[1].runs+"("+m.batsmen[1].balls+")"
 
-document.getElementById("bowler").innerText = m.bowler.name
-
-updateRates(m)
+document.getElementById("bowler").innerText=
+m.bowler.name+" | "+m.currentOverRuns+" runs this over"
 
 renderBalls(m)
 
+updateRates(m)
+
+showAnimation(m)
+
 })
-
-function updateRates(m){
-
-let rrBox = document.getElementById("rr")
-let reqBox = document.getElementById("req")
-
-let ballsPlayed = (m.overs * 6) + m.balls
-
-if(m.innings == 1){
-
-reqBox.innerText = ""
-
-if(ballsPlayed > 0){
-
-let rr = (m.runs / (ballsPlayed/6)).toFixed(2)
-
-rrBox.innerText = "RR: " + rr
-
-}
-
-}
-
-if(m.innings == 2){
-
-rrBox.innerText = ""
-
-let ballsLeft = (m.totalOvers * 6) - ballsPlayed
-
-let runsNeeded = m.target - m.runs
-
-if(runsNeeded < 0) runsNeeded = 0
-
-if(ballsLeft > 0){
-
-let rrr = (runsNeeded / (ballsLeft/6)).toFixed(2)
-
-reqBox.innerText =
-"Need " + runsNeeded +
-" runs from " + ballsLeft +
-" balls | RRR " + rrr
-
-}
-
-}
-
-}
 
 function renderBalls(m){
 
-let box = document.getElementById("balls")
+let box=document.getElementById("balls")
 
-box.innerHTML = ""
+box.innerHTML=""
 
-m.lastBalls.forEach(b => {
+m.lastBalls.forEach(b=>{
 
-box.innerHTML += `<span>${b}</span>`
+box.innerHTML+=`<span>${b}</span>`
 
 })
+
+}
+
+function updateRates(m){
+
+let rrBox=document.getElementById("rr")
+let reqBox=document.getElementById("req")
+
+let ballsPlayed=(m.overs*6)+m.balls
+
+if(m.innings==1){
+
+reqBox.innerText=""
+
+if(ballsPlayed>0){
+
+let rr=(m.runs/(ballsPlayed/6)).toFixed(2)
+
+rrBox.innerText="RR: "+rr
+
+}
+
+}
+
+if(m.innings==2){
+
+rrBox.innerText=""
+
+let ballsLeft=(m.totalOvers*6)-ballsPlayed
+
+let runsNeeded=m.target-m.runs
+
+if(runsNeeded<0) runsNeeded=0
+
+if(ballsLeft>0){
+
+let rrr=(runsNeeded/(ballsLeft/6)).toFixed(2)
+
+reqBox.innerText=
+
+"Need "+runsNeeded+
+" runs from "+ballsLeft+
+" balls | RRR "+rrr
+
+}
+
+}
+
+}
+
+function showAnimation(m){
+
+let box=document.getElementById("anim")
+
+if(m.anim){
+
+box.innerText=m.anim
+
+box.classList.add("show")
+
+setTimeout(()=>{
+
+box.classList.remove("show")
+
+},1500)
+
+}
 
 }
